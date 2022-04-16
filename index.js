a8ab14c6be463c2abf9baacfb71cfffc593e4dc2
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2022-04-16 15:05:46 -0400

    commit tree 0ace185bdf22a42118047d4f1ad3e757a6577366

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 // Write your solution in this file!
+
 const employee = {
-  name: 'osvaldo',
-  streetAddress: 'East 23rd'
+  name: "osvaldo",
+  streetAddress: "Union Sq",
 };
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
+function updateEmployeeWithKeyAndValue(employee,key,value){
   const newEmployee = {...employee};
   newEmployee[key] = value;
   return newEmployee;
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-  employee[key] = value
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+  employee[key] = value;
   return employee;
 }
 
-function deleteFromEmployeeByKey(employee, key) {
-  const newObject = Object.assign({}, employee)
-  delete newObject[key]
+function deleteFromEmployeeByKey(employee,key) {
+  const newObject = Object.assign({},employee);
+  delete newObject[key];
   return newObject;
 }
 
 function destructivelyDeleteFromEmployeeByKey(employee, key){
-  delete employee [key]
+  delete employee[key];
   return employee;
 }
